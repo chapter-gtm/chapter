683c7353f87979c5444a00614bbd58b6ca02f255
shri <shrirang@bhate.de>
2024-02-14 19:08:25 +0100

    Add header to survey form

--- src/components/SurveyForm.tsx
+++ src/components/SurveyForm.tsx
@@ -72,94 +72,102 @@ export function SurveyForm({ survey }: SurveyFormProps) {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="goal"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                {" "}
-                What do you want to find out? (Research Goal)
-              </FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="e.g. Product teams have a strong unmet need to get deep insights at a quicker speed"
-                  className="resize-none"
-                  {...field}
-                  value={
-                    survey !== null && survey.objective !== undefined
-                      ? survey.objective
-                      : ""
-                  }
-                ></Textarea>
-              </FormControl>
-              <FormDescription>
-                This is the goal of your research or the hypothesis you&apos;re
-                testing.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="starter_question"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                {" "}
-                How do you want to start? (First question to the participant)
-              </FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="e.g. Tell me a little about your last user-interview. This can include the planning, conducting, and review."
-                  className="resize-none"
-                  {...field}
-                  value={
-                    survey !== null &&
-                    survey.structure !== undefined &&
-                    survey.structure.starter_questions.length > 0
-                      ? survey.structure.starter_questions[0]
-                      : ""
-                  }
-                />
-              </FormControl>
-              <FormDescription>
-                This will be the interview starter question. It sets the
-                direction of your interview.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="target_personas"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel> Who&apos;s your target persona? </FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="e.g. Product Managers, UX Researchers"
-                  {...field}
-                  value={
-                    survey !== null && survey.participant !== undefined
-                      ? survey.participant.personas.join(", ")
-                      : ""
-                  }
-                ></Input>
-              </FormControl>
-              <FormDescription>
-                Role name or description of your target audience.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Create survey</Button>
-      </form>
-    </Form>
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Survey Definition</h3>
+        <p className="text-sm text-muted-foreground">
+          This is your survey definition.
+        </p>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="goal"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  {" "}
+                  What do you want to find out? (Research Goal)
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="e.g. Product teams have a strong unmet need to get deep insights at a quicker speed"
+                    className="resize-none"
+                    {...field}
+                    value={
+                      survey !== null && survey.objective !== undefined
+                        ? survey.objective
+                        : ""
+                    }
+                  ></Textarea>
+                </FormControl>
+                <FormDescription>
+                  This is the goal of your research or the hypothesis
+                  you&apos;re testing.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="starter_question"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  {" "}
+                  How do you want to start? (First question to the participant)
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="e.g. Tell me a little about your last user-interview. This can include the planning, conducting, and review."
+                    className="resize-none"
+                    {...field}
+                    value={
+                      survey !== null &&
+                      survey.structure !== undefined &&
+                      survey.structure.starter_questions.length > 0
+                        ? survey.structure.starter_questions[0]
+                        : ""
+                    }
+                  />
+                </FormControl>
+                <FormDescription>
+                  This will be the interview starter question. It sets the
+                  direction of your interview.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="target_personas"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Who&apos;s your target persona? </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="e.g. Product Managers, UX Researchers"
+                    {...field}
+                    value={
+                      survey !== null && survey.participant !== undefined
+                        ? survey.participant.personas.join(", ")
+                        : ""
+                    }
+                  ></Input>
+                </FormControl>
+                <FormDescription>
+                  Role name or description of your target audience.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Create survey</Button>
+        </form>
+      </Form>
+    </div>
   );
 }
