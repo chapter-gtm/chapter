fa1904fa61c22b72f236e379c60e09a449676e87
shri <shrirang@bhate.de>
2024-02-17 16:31:38 +0100

    Remove extra border

--- src/components/project/ProjectDetails.tsx
+++ src/components/project/ProjectDetails.tsx
@@ -38,40 +38,35 @@ export async function ProjectDetails({ id }: ProjectDetailsProps) {
         <ErrorMessage />
       ) : (
         <div className="hidden md:block">
-          <div className="border-t">
-            <div className="bg-background">
-              <div className="grid lg:grid-cols-5">
-                <div className="col-span-3 lg:col-span-4 lg:border-l">
-                  <div className="h-full px-4 py-6 lg:px-8">
-                    <Tabs
-                      defaultValue="definition"
-                      className="h-full space-y-6"
+          <div className="bg-background">
+            <div className="grid lg:grid-cols-5">
+              <div className="col-span-3 lg:col-span-4">
+                <div className="h-full px-4 py-6 lg:px-8">
+                  <Tabs defaultValue="definition" className="h-full space-y-6">
+                    <div className="space-between flex items-center">
+                      <TabsList>
+                        <TabsTrigger value="definition" className="relative">
+                          Definition
+                        </TabsTrigger>
+                        <TabsTrigger value="results">Results</TabsTrigger>
+                        <TabsTrigger value="insights" disabled>
+                          Insights
+                        </TabsTrigger>
+                      </TabsList>
+                    </div>
+                    <TabsContent
+                      value="definition"
+                      className="border-none p-0 outline-none"
                     >
-                      <div className="space-between flex items-center">
-                        <TabsList>
-                          <TabsTrigger value="definition" className="relative">
-                            Definition
-                          </TabsTrigger>
-                          <TabsTrigger value="results">Results</TabsTrigger>
-                          <TabsTrigger value="insights" disabled>
-                            Insights
-                          </TabsTrigger>
-                        </TabsList>
-                      </div>
-                      <TabsContent
-                        value="definition"
-                        className="border-none p-0 outline-none"
-                      >
-                        <ProjectDefinition project={project} />
-                      </TabsContent>
-                      <TabsContent
-                        value="results"
-                        className="h-full flex-col border-none p-0 data-[state=active]:flex"
-                      >
-                        <ProjectResults project={project} />
-                      </TabsContent>
-                    </Tabs>
-                  </div>
+                      <ProjectDefinition project={project} />
+                    </TabsContent>
+                    <TabsContent
+                      value="results"
+                      className="h-full flex-col border-none p-0 data-[state=active]:flex"
+                    >
+                      <ProjectResults project={project} />
+                    </TabsContent>
+                  </Tabs>
                 </div>
               </div>
             </div>
