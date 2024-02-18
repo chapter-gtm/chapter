3a613da3e7eb4b42f28af93e202ada7361500f8d
shri <shrirang@bhate.de>
2024-02-18 16:54:56 +0100

    Remove surveys

--- src/app/surveys/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SurveyScreen } from "@/components/survey/SurveyScreen";
-
-export default async function SurveyPage() {
-  return (
-    <div className="hidden flex-col md:flex">
-      <SurveyScreen />
-    </div>
-  );
-}
