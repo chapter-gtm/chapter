ac9965bc375fdc55cd6698464df328439707b47d
shri <shrirang@bhate.de>
2024-03-07 13:10:09 +0100

    Fix project response full

--- src/components/project/ProjectResponseDetailsFull.tsx
+++ src/components/project/ProjectResponseDetailsFull.tsx
@@ -22,17 +22,18 @@ import {
   Cross1Icon,
   RocketIcon,
 } from "@radix-ui/react-icons";
-
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { ErrorMessage } from "@/components/ErrorMessage";
 import { ProjectResponseTranscript } from "@/components/project/ProjectResponseTranscript";
-import { PropList } from "./PropList";
+import { ProjectResponsePropList } from "@/components/project/ProjectResponsePropList";
 
 async function getProjectResponse(
   projectId: string,
   projectResponseId: string,
 ) {
   const jwtToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDk4MDYwOTQsInN1YiI6InRlc3RAbmVjdGFyLnJ1biIsImlhdCI6MTcwOTcxOTY5NCwiZXh0cmFzIjp7fX0.1bjE2vGjg1gV1B_8oE-h80YX3-lfSA3W07vhtAFxRy8";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDk4OTU3NzgsInN1YiI6InRlc3RAbmVjdGFyLnJ1biIsImlhdCI6MTcwOTgwOTM3OCwiZXh0cmFzIjp7fX0.f1reY5_k-8m5tRU9G9Y5ZVVgfQpgV8wEyQb7kyknyyg";
   const response = await fetch(
     "http://localhost:8000/api/projects/" +
       projectId +
@@ -81,61 +82,80 @@ export function ProjectResponseDetailsFull({
   }, [projectId, projectResponseId, projectResponse]);
 
   return (
-    <div className="grid grid-cols-4 justify-start content-start">
-      <div className="col-span-3 border-e border-slate-100 h-dvh">
-        <div className="flex-1 border-b border-slate-100 pb-3 ">
-          <div className="flex flex-row justify-between w-full py-3 px-7 justify-start">
-            <div className="flex gap-4 items-center">
-              <ToggleGroup type="single">
-                <ToggleGroupItem value="a" className="border-slate-200 border">
-                  <Cross1Icon className="w-3 h-3" />
-                </ToggleGroupItem>
-                <ToggleGroupItem value="a" className="border-slate-200 border">
-                  <ChevronDownIcon className="w-3 h-3" />
-                </ToggleGroupItem>
-                <ToggleGroupItem value="b" className="border-slate-200 border">
-                  <ChevronUpIcon className="w-3 h-3" />
-                </ToggleGroupItem>
-              </ToggleGroup>
-              <p className="text-sm font-medium text-slate-600">
-                Respondant #312
-              </p>
-            </div>
-            <div className="flex gap-4 items-center">
-              <button className="border-slate-200 border p-2 rounded-lg hover:bg-slate-100">
-                <Link2Icon className="w-4 h-4" />
-              </button>
-            </div>
-          </div>
-          <div className="w-2/3 mx-auto py-3 px-7 mt-12">
-            <h1 className="text-3xl font-semibold text-slate-600">Unknown</h1>
-          </div>
-        </div>
-        <div className="flex w-full">
-          <Tabs defaultValue="transcript" className="w-full ">
-            <div className="w-full flex flex-row border-b border-slate-100 justify-between py-3 items-center">
-              <div className="w-2/3 mx-auto">
-                <TabsList className="grid w-full grid-cols-2 w-[200px]">
-                  <TabsTrigger value="transcript">Transcript</TabsTrigger>
-                  <TabsTrigger value="password">Activity</TabsTrigger>
-                </TabsList>
+    <>
+      {response !== null ? (
+        <div className="grid grid-cols-4 justify-start content-start">
+          <div className="col-span-3 border-e border-slate-100 h-dvh">
+            <div className="flex-1 border-b border-slate-100 pb-3 ">
+              <div className="flex flex-row justify-between w-full py-3 px-7 justify-start">
+                <div className="flex gap-4 items-center">
+                  <ToggleGroup type="single">
+                    <ToggleGroupItem
+                      value="a"
+                      className="border-slate-200 border"
+                    >
+                      <Cross1Icon className="w-3 h-3" />
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                      value="a"
+                      className="border-slate-200 border"
+                    >
+                      <ChevronDownIcon className="w-3 h-3" />
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                      value="b"
+                      className="border-slate-200 border"
+                    >
+                      <ChevronUpIcon className="w-3 h-3" />
+                    </ToggleGroupItem>
+                  </ToggleGroup>
+                  <p className="text-sm font-medium text-slate-600">
+                    Respondant #312
+                  </p>
+                </div>
+                <div className="flex gap-4 items-center">
+                  <button className="border-slate-200 border p-2 rounded-lg hover:bg-slate-100">
+                    <Link2Icon className="w-4 h-4" />
+                  </button>
+                </div>
+              </div>
+              <div className="w-2/3 mx-auto py-3 px-7 mt-12">
+                <h1 className="text-3xl font-semibold text-slate-600">
+                  Unknown
+                </h1>
               </div>
             </div>
-            <TabsContent value="transcript">
-              <div className="w-2/3 mx-auto">
-                {response !== null && (
-                  <ProjectResponseTranscript projectResponse={response} />
-                )}
-              </div>
-            </TabsContent>
-          </Tabs>
-        </div>
-      </div>
+            <div className="flex w-full">
+              <Tabs defaultValue="transcript" className="w-full ">
+                <div className="w-full flex flex-row border-b border-slate-100 justify-between py-3 items-center">
+                  <div className="w-2/3 mx-auto">
+                    <TabsList className="grid w-full grid-cols-2 w-[200px]">
+                      <TabsTrigger value="transcript">Transcript</TabsTrigger>
+                      <TabsTrigger value="password">Activity</TabsTrigger>
+                    </TabsList>
+                  </div>
+                </div>
+                <TabsContent value="transcript">
+                  <div className="w-2/3 mx-auto">
+                    {response !== null && (
+                      <ProjectResponseTranscript projectResponse={response} />
+                    )}
+                  </div>
+                </TabsContent>
+              </Tabs>
+            </div>
+          </div>
 
-      <div className="col-span-1 pt-2 px-6 space-y-5">
-        <ProjectResponseIdentity />
-        <PropList />
-      </div>
-    </div>
+          <div className="col-span-1 pt-2 px-6 space-y-5">
+            <ProjectResponseIdentity />
+            {response !== null && (
+              <ProjectResponsePropList projectResponse={response} />
+            )}
+          </div>
+        </div>
+      ) : (
+        <ErrorMessage />
+      )}
+    </>
   );
 }
