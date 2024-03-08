52ee9a82b1a03a2e185650a2d3a1b847e2233cc8
shri <shrirang@bhate.de>
2024-03-08 13:03:04 +0100

    Fix generic error message

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -5,9 +5,9 @@ export function ErrorMessage() {
     <div className="flex h-[450px] shrink-0 items-center justify-center rounded-md border border-dashed">
       <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
         <ServerCrash />
-        <h3 className="mt-4 text-lg font-semibold">Something went wrong!</h3>
+        <h3 className="mt-4 text-lg font-semibold">Something went wrong</h3>
         <p className="mb-4 mt-2 text-sm text-muted-foreground">
-          Please contact support, they&apos;ll sort it out for you!
+          Please contact support, we&apos;ll sort it out for you!
         </p>
       </div>
     </div>
