56adc1c89b5b193561195aca8d1670e081bce575
shri <shrirang@bhate.de>
2024-04-13 19:51:19 +0200

    Add favicon

--- src/app/(console)/layout.tsx
+++ src/app/(console)/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <div className="bg-zinc-100 h-screen flex w-full">
           <div className="flex flex-1 overflow-hidden">
