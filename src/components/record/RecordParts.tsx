07a808f1b1b1c5caacd00516298b09ad78e682bd
shri <shrirang@bhate.de>
2024-04-24 18:21:31 +0200

    Add record components

--- src/components/record/RecordParts.tsx
+++ src/components/record/RecordParts.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "@/components/ui/separator";
 import { DataRecord } from "@/types/record";
 
 interface RecordPartsProps {
@@ -5,5 +6,18 @@ interface RecordPartsProps {
 }
 
 export function RecordParts({ record }: RecordPartsProps) {
-  return <></>;
+  return (
+    <>
+      {record.parts.map((part, partIndex) => (
+        <div className="flex flex-col gap-y-3 p-3" key={partIndex}>
+          <div className="max-w-[75%] px-3 py-2 text-xs text-slate-500 ml-0">
+            {part.author}
+          </div>
+          <div className=" max-w-[75%] gap-2 rounded-lg px-3 py-2 text-sm bg-white">
+            {part.body}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
