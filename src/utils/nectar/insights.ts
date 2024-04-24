d01b612d6630abee8a381ab471a8162935e597ba
shri <shrirang@bhate.de>
2024-04-24 18:21:18 +0200

    Add insights api call

--- src/utils/nectar/insights.ts
+++ src/utils/nectar/insights.ts
@@ -3,6 +3,50 @@ import { type Insight } from "@/types/insight";
 
 const NECTAR_API_BASE = "https://api.nectar.run/api";
 
+export async function getInsights(
+  token: string,
+  pageSize: number,
+  currentPage: number,
+) {
+  const response = await fetch(
+    NECTAR_API_BASE +
+      "/insights?" +
+      new URLSearchParams({
+        pageSize: pageSize.toString(),
+        currentPage: currentPage.toString(),
+      }),
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  if (!response.ok) {
+    const msg = await response.json();
+    throw new Error(msg?.detail);
+  }
+  const data = await response.json();
+  const insights = "items" in data ? (data["items"] as Insight[]) : [];
+  return insights;
+}
+
+export async function getInsight(token: string, id: string) {
+  const response = await fetch(NECTAR_API_BASE + "/insights/" + id, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    const msg = await response.json();
+    throw new Error(msg?.detail);
+  }
+  const data = await response.json();
+  const insight = data as Insight;
+  return insight;
+}
+
 export async function generateInsights(token: string, recordIds: string[]) {
   const response = await fetch(NECTAR_API_BASE + "/insights/generate/", {
     method: "POST",
