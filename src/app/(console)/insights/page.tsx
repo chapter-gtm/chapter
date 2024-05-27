cb7982e1bb764957f4c55f141a7ef710378181e8
shri <shrirang@bhate.de>
2024-05-27 14:11:55 +0200

    Fix build errors

--- src/app/(console)/insights/page.tsx
+++ src/app/(console)/insights/page.tsx
@@ -2,37 +2,35 @@
 
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import { Metadata } from "next";
+import { Suspense } from "react";
 import { EmptySelectionCard } from "@/components/EmptySelectionCard";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Insights } from "@/components/insight/Insights";
 import { Records } from "@/components/record/Records";
 
 export default function InsightsPage() {
-  const searchParams = useSearchParams();
-  let tabName = searchParams.get("tab") || "conversations";
+  function InsightTabs() {
+    const searchParams = useSearchParams();
+    let tabName = searchParams.get("tab") || "conversations";
 
-  if (!["conversations", "insights"].includes(tabName)) {
-    tabName = "conversations";
-  }
+    if (!["conversations", "insights"].includes(tabName)) {
+      tabName = "conversations";
+    }
 
-  return (
-    <>
+    return (
       <div className="w-full space-y-2 px-6 mt-2">
         <Tabs defaultValue={tabName} className="h-dvh flex flex-col ">
           <div className="space-between basis-1/3 ">
-            <div className="flex flex-col justify-start">
-              <div className="flex flex-row items-center justify-start px-6 mt-1">
-                <TabsList className="bg-zinc-200 font-normal flex">
-                  <TabsTrigger value="conversations" className="relative">
-                    Conversations
-                  </TabsTrigger>
-                  <TabsTrigger value="insights">Custom Insights</TabsTrigger>
-                </TabsList>
-              </div>
+            <div className="flex items-center justify-center">
+              <TabsList className="bg-zinc-200 font-normal">
+                <TabsTrigger value="conversations" className="relative">
+                  Conversations
+                </TabsTrigger>
+                <TabsTrigger value="insights">Custom Insights</TabsTrigger>
+              </TabsList>
               <TabsContent
                 value="conversations"
-                className="mt-0 data-[state=active]:flex flex-col h-full pb-32 px-6 pt-4"
+                className="mt-0 data-[state=active]:flex flex-col h-full pb-32 px-6"
               >
                 <Records />
               </TabsContent>
@@ -47,6 +45,14 @@ export default function InsightsPage() {
           </div>
         </Tabs>
       </div>
+    );
+  }
+
+  return (
+    <>
+      <Suspense>
+        <InsightTabs />
+      </Suspense>
     </>
   );
 }
