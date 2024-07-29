02dbf5d29a26ad12824647d621381b621db71ae3
shri <shrirang@bhate.de>
2024-07-29 16:33:41 +0200

    Add tenant and company routes

--- src/app/server/routers.py
+++ src/app/server/routers.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from app.domain.accounts.controllers import AccessController, UserController, UserRoleController
+from app.domain.accounts.controllers import AccessController, UserController, UserRoleController, TenantController
 from app.domain.system.controllers import SystemController
 from app.domain.tags.controllers import TagController
 from app.domain.teams.controllers import TeamController, TeamMemberController
+from app.domain.companies.controllers import CompanyController
 
 if TYPE_CHECKING:
     from litestar.types import ControllerRouterHandler
@@ -20,5 +21,7 @@ route_handlers: list[ControllerRouterHandler] = [
     #  TeamInvitationController,
     TeamMemberController,
     TagController,
+    TenantController,
+    CompanyController,
     SystemController,
 ]
