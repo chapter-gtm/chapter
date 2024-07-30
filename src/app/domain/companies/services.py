8c799eadc8904de92451d78c7993391b165aa55b
shri <shrirang@bhate.de>
2024-07-30 12:55:26 +0200

    Code cleanup

--- src/app/domain/companies/services.py
+++ src/app/domain/companies/services.py
@@ -4,12 +4,10 @@ from typing import TYPE_CHECKING, Any
 
 from advanced_alchemy.exceptions import RepositoryError
 from advanced_alchemy.service import SQLAlchemyAsyncRepositoryService, is_dict, is_msgspec_model, is_pydantic_model
-from advanced_alchemy.utils.text import slugify
 from uuid_utils.compat import uuid4
 
 from app.lib.schema import CamelizedBaseStruct, Location, Funding
 from app.db.models import Company
-from app.db.models.user import User  # noqa: TCH001
 
 from .repositories import CompanyRepository
 
@@ -37,32 +35,6 @@ class CompanyService(SQLAlchemyAsyncRepositoryService[Company]):
     def __init__(self, **repo_kwargs: Any) -> None:
         self.repository: CompanyRepository = self.repository_type(**repo_kwargs)
         self.model_type = self.repository.model_type
-
-    async def create(
-        self,
-        data: ModelDictT[Company],
-        *,
-        load: LoadSpec | None = None,
-        execution_options: dict[str, Any] | None = None,
-        auto_commit: bool | None = None,
-        auto_expunge: bool | None = None,
-        auto_refresh: bool | None = None,
-    ) -> Company:
-        """Create a new company."""
-        if isinstance(data, dict):
-            data = await self.to_model(data, "create")
-            print("Model:")
-            print(data)
-            print(data.hq_location)
-        return await super().create(
-            data=data,
-            load=load,
-            execution_options=execution_options,
-            auto_commit=auto_commit,
-            auto_expunge=auto_expunge,
-            auto_refresh=auto_refresh,
-        )
-
 
     async def to_model(self, data: Company | dict[str, Any] | Struct, operation: str | None = None) -> Company:
         if (is_msgspec_model(data) or is_pydantic_model(data)) and operation == "create" and data.slug is None:  # type: ignore[union-attr]
