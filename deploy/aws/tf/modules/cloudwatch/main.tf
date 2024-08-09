195bdaf29122557f6b7b123c3094de5d2f33430d
shri <shrirang@bhate.de>
2024-08-09 15:39:40 +0200

    Add log groups for db-migrator and worker

--- deploy/aws/tf/modules/cloudwatch/main.tf
+++ deploy/aws/tf/modules/cloudwatch/main.tf
@@ -5,7 +5,27 @@ resource "aws_cloudwatch_log_group" "app_log_group" {
   retention_in_days = 7
 
   tags = {
-    Name = "${var.app_name}-log-group-${var.environment}"
+    Name = "${var.app_name}-app-log-group-${var.environment}"
+    Env  = var.environment
+  }
+}
+
+resource "aws_cloudwatch_log_group" "db_migrator_log_group" {
+  name              = "/ecs/${var.app_name}-db-migrator-${var.environment}"
+  retention_in_days = 7
+
+  tags = {
+    Name = "${var.app_name}-db-migrator-log-group-${var.environment}"
+    Env  = var.environment
+  }
+}
+
+resource "aws_cloudwatch_log_group" "worker_log_group" {
+  name              = "/ecs/${var.app_name}-worker-${var.environment}"
+  retention_in_days = 7
+
+  tags = {
+    Name = "${var.app_name}-worker-log-group-${var.environment}"
     Env  = var.environment
   }
 }
