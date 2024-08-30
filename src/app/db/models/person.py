bdedeaeb167b7612041c366f31b963059db525e1
shri <shrirang@bhate.de>
2024-08-30 13:01:48 +0200

    Make person work email a sting instead of a list

--- src/app/db/models/person.py
+++ src/app/db/models/person.py
@@ -16,7 +16,17 @@ class Person(UUIDAuditBase, SlugKey):
     """A person."""
 
     __tablename__ = "person"
-    __pii_columns__ = {"first_name", "last_name", "full_name", "linkedin_url", "profile_pic_url", "personal_emails", "work_emails", "personal_numbers", "social_activities"}
+    __pii_columns__ = {
+        "first_name",
+        "last_name",
+        "full_name",
+        "linkedin_url",
+        "profile_pic_url",
+        "personal_emails",
+        "work_emails",
+        "personal_numbers",
+        "social_activities",
+    }
     first_name: Mapped[str] = mapped_column(nullable=True, default=None)
     last_name: Mapped[str] = mapped_column(nullable=True, default=None)
     full_name: Mapped[str] = mapped_column(nullable=True, default=None)
@@ -31,13 +41,17 @@ class Person(UUIDAuditBase, SlugKey):
     github_profile_url: Mapped[str | None] = mapped_column(String(length=2083), nullable=True, default=None)
     location: Mapped[Location | None] = mapped_column(LocationType, nullable=True, default=None)
     personal_emails: Mapped[list[str] | None] = mapped_column(JSONB, nullable=True, default=None)
-    work_emails: Mapped[list[str] | None] = mapped_column(JSONB, nullable=True, default=None)
+    work_email: Mapped[str | None] = mapped_column(nullable=True, default=None)
     personal_numbers: Mapped[list[str] | None] = mapped_column(JSONB, nullable=True, default=None)
     birth_date: Mapped[date | None] = mapped_column(nullable=True, default=None)
     gender: Mapped[str | None] = mapped_column(nullable=True, default=None)
     languages: Mapped[list[str] | None] = mapped_column(JSONB, nullable=True, default=None)
-    work_experiences: Mapped[list[WorkExperience] | None] = mapped_column(WorkExperienceType, nullable=True, default=None)
-    social_activities: Mapped[list[SocialActivity] | None] = mapped_column(SocialActivityType, nullable=True, default=None)
+    work_experiences: Mapped[list[WorkExperience] | None] = mapped_column(
+        WorkExperienceType, nullable=True, default=None
+    )
+    social_activities: Mapped[list[SocialActivity] | None] = mapped_column(
+        SocialActivityType, nullable=True, default=None
+    )
     # -----------
     # ORM Relationships
     # ------------
