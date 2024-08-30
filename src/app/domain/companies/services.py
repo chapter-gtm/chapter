51c8d528bda44aa2084845fe38693ac501269ae6
shri <shrirang@bhate.de>
2024-08-30 17:54:12 +0200

    Fix slug generation on upsert

--- src/app/domain/companies/services.py
+++ src/app/domain/companies/services.py
@@ -88,8 +88,12 @@ class CompanyService(SQLAlchemyAsyncRepositoryService[Company]):
             data.slug = await self.repository.get_available_slug(data.name)  # type: ignore[union-attr]
         if (is_msgspec_model(data) or is_pydantic_model(data)) and operation == "update" and data.slug is None:  # type: ignore[union-attr]
             data.slug = await self.repository.get_available_slug(data.name)  # type: ignore[union-attr]
+        if (is_msgspec_model(data) or is_pydantic_model(data)) and operation == "upsert" and data.slug is None:  # type: ignore[union-attr]
+            data.slug = await self.repository.get_available_slug(data.name)  # type: ignore[union-attr]
         if is_dict(data) and "slug" not in data and operation == "create":
             data["slug"] = await self.repository.get_available_slug(data["name"])
         if is_dict(data) and "slug" not in data and "name" in data and operation == "update":
+            data["slug"] = await self.repository.get_available_slug(data["name"])
+        if is_dict(data) and "slug" not in data and "name" in data and operation == "upsert":
             data["slug"] = await self.repository.get_available_slug(data["name"])
         return await super().to_model(data, operation)
