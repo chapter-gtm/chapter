53d6a926f46bc2c6e7b37657b8e3ae3e2c03bd97
shri <shrirangbhate@yahoo.com>
2024-11-15 19:33:09 +0100

    Add default values for org size

--- src/components/AgentForm.tsx
+++ src/components/AgentForm.tsx
@@ -58,24 +58,28 @@ const agentFormSchema = z.object({
         .number({ invalid_type_error: "Min must be a number" })
         .min(1, "Min must be greater than 0")
         .max(10000, "Max must not be greater than 10,000")
-        .int(),
+        .int()
+        .default(1),
       headcountMax: z
         .number({ invalid_type_error: "Max must be a number" })
         .min(1, "Max must be greater than 0 and min")
         .max(10000, "Max must not be greater than 10,000")
-        .int(),
+        .int()
+        .default(10000),
       orgSize: z
         .object({
           engineeringMin: z
             .number({ invalid_type_error: "Min must be a number" })
             .min(1, "Min must be greater than 0")
             .max(1000, "Max must not be greater than 1000")
-            .int(),
+            .int()
+            .default(1),
           engineeringMax: z
             .number({ invalid_type_error: "Max must be a number" })
             .min(1, "Max must be greater than 0 and min")
             .max(1000, "Max must not be greater than 1000")
-            .int(),
+            .int()
+            .default(100),
         })
         .refine((data) => data.engineeringMax > data.engineeringMin, {
           message: "Max must be greater than Min",
