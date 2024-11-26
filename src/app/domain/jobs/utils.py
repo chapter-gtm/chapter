7cb195af5f9dc464f25b3827176a8bd31d6137c0
shri <shrirangbhate@yahoo.com>
2024-11-26 19:09:59 +0100

    Normalise tool names

--- src/app/domain/jobs/utils.py
+++ src/app/domain/jobs/utils.py
@@ -3,9 +3,64 @@ import json
 import structlog
 from typing import Any
 from openai import AsyncOpenAI
+from rapidfuzz import process, fuzz
 
 
 logger = structlog.get_logger()
+
+canonical_tech_names = [
+    "Python",
+    "Kubernetes",
+    "JavaScript",
+    "Node.js",
+    "React",
+    "Flask",
+    "GitHub",
+    "Github Actions",
+    "Jenkins",
+    "CircleCI",
+    "GitLab",
+    "GitLab CI",
+    "Docker",
+    "GitOps",
+    "Argo CD",
+    "Cypress",
+    "Playwright",
+    "Rust",
+    "Python",
+    "TensorFlow",
+    "PyTorch",
+    "LlamaIndex",
+    "LangCHain",
+    "LLMs",
+    "RAG",
+    "HuggingFace",
+    "OpenAI",
+    "APIs",
+    "SDKs",
+    "REST APIs",
+    "GraphQL",
+    "OpenAPI",
+    "Markdown",
+    "Authorization",
+    "Access Management",
+    "AWS IAM",
+    "Django",
+    "Spring Boot",
+    "Flask",
+    "Express",
+    "Ruby on Rails",
+    "SailPoint",
+    "Okta",
+    "Auth0",
+]
+
+special_cases = {
+    "k8s": "Kubernetes",
+    "js": "Javascript",
+    "ts": "Typescript",
+    "node": "Node.js",
+}
 
 model = os.environ["OPENAI_MODEL_NAME"]
 client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
@@ -48,6 +103,28 @@ prompt = """
 """
 
 
+def normalise_tool_stack(tool_stack):
+    def normalise_name(name):
+        # Check case-insensitive special cases
+        lower_name = name.lower()
+        if lower_name in special_cases:
+            return special_cases[lower_name]
+
+        # Fuzzy matching for general cases
+        result = process.extractOne(name, canonical_tech_names, scorer=fuzz.ratio)
+        if result:
+            match, score = result[0], result[1]
+            return match if score > 80 else name
+
+        return name
+
+    # Normalize each item's name in the tech stack
+    for item in tool_stack:
+        if "name" in item:
+            item["name"] = normalise_name(item["name"].strip())
+    return tool_stack
+
+
 async def extract_job_details_from_html(html_content: str) -> dict[str, Any]:
     """Extracts job post from the html using an LLM."""
     messages = [
@@ -72,5 +149,11 @@ async def extract_job_details_from_html(html_content: str) -> dict[str, Any]:
         and ("url" not in job_details.get("company") or "linkedin_url" not in job_details.get("company"))
     ):
         logger.warn("Failed to extract necessary information from job post", job_details=job_details)
+
+    # Normalise tools
+    try:
+        job_details["tools"] = normalise_tool_stack(job_details["tools"])
+    except Exception as e:
+        logger.warn("Failed to normalise tool stack", job_details=job_details, exc_info=e)
 
     return job_details
