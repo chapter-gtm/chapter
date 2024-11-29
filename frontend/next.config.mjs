9f9d608ef563a63511abf2ff05f13982d9762266
shri <shrirangbhate@yahoo.com>
2024-11-29 13:32:44 +0100

    Add frontend to docker compose and add 3rd party api key variables to example env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["img.logo.dev"], // Add the domain you want to allow
   },
-};
+  output: "standalone",
+}
 
-export default nextConfig;
+export default nextConfig
