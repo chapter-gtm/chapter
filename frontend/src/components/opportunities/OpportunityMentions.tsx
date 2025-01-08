7c00efbb2d100fdc9abc076aafa4834988aebd4f
shri <shrirangbhate@yahoo.com>
2025-01-08 11:09:14 +0100

    Remove quotes from the markup

--- frontend/src/components/opportunities/OpportunityMentions.tsx
+++ frontend/src/components/opportunities/OpportunityMentions.tsx
@@ -98,7 +98,7 @@ export function OpportunityMentions({
                           <li className="col-span-1 border-l-2 px-3 border-secondary-foreground/50">
                             {/* <span className="min-w-1 h-auto bg-primary rounded-lg"></span> */}
                             <p className="text-base dark:text-secondary font-light">
-                              "{jobPostContext.sentence}"
+                              {jobPostContext.sentence}
                             </p>
                           </li>
                         </ul>
